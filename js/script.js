972f1eda18e34c0b50283af57ec7271ab7a0a7cd
andrewchosen <andrew.chosen@gmail.com>
2016-03-07 15:45:44 -0800

    Added some styles

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
-﻿// ›
+﻿// Start
 $('document').ready(function(){
+
+	// Portfolio item information sliding effect
 	$('li').hover(function(){
 		$(this).find('div').slideToggle(300);
 	});
+
+
 });
