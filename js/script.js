b291991e6050c96008c086fb03f270f9c0163484
andrewchosen <andrew.chosen@gmail.com>
2016-03-09 11:51:43 -0800

    portfolio info now auto shows on mobile

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-﻿// Start
+﻿/**
+ * ON PAGE LOAD
+ **/
+
 $('document').ready(function(){
 
-	// Portfolio item information sliding effect
-	$('li').hover(function(){
-		$(this).find('div').stop(true, true).slideToggle(300);
-	});
-
+	if($('#main').width() >= 768){
+		$('li').find('div').hide();
+		// Portfolio item information sliding effect
+		$('li').hover(function(){
+			$(this).find('div').stop(true, true).slideToggle(300);
+		});
+	}
 
 });
